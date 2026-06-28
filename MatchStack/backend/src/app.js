2553cc87c5f460801a828ff438a6605edbcb3478
Arbaaz Makandar <30730654+arbaazmakandar@users.noreply.github.com>
2026-06-28 23:05:10 +0530

    exploring get post delete

--- MatchStack/backend/src/app.js
+++ MatchStack/backend/src/app.js
@@ -2,9 +2,40 @@ const express = require("express");
 
 const app = express(); // Instance of expressJS application
 
+app.use((req, res) => {
+  res.send("Hello from expressJS");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "John", lastName: "Doe" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Data is posted successfully");
+  res.send("Data is posted successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data is deleted successfully");
+});
+
+//Order matters in expressJS,
+app.use("/hello/2", (req, res) => {
+  res.send("Hello 2");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello ");
+});
+
 //This function is called as request handler
 app.use("/test", (req, res) => {
-  res.send("Hello from the server2");
+  res.send("test");
+});
+
+//This will match to all GET,POST,PUT,DELETE requests to the root path
+app.use("/", (req, res) => {
+  res.send("dashboard");
 });
 
 app.listen(3000, () => {
